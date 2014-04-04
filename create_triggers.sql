6f310194e101e5709b413a16a0c8572c524ca50d
Helga Lis <oklyky@gmail.com>
2014-04-04 21:01:36 +0400

    Update create_triggers.sql
    добавлены триггеры связанные с покупателями и проверка условия на нуль но нужно проверить

--- create_triggers.sql
+++ create_triggers.sql
@@ -2,14 +2,45 @@ create generator g_books;
 create generator g_authors;
 create generator g_books_categories;
 create generator g_books_authors;
+create generator g_customers;
+create generator g_customers_orders;
+create generator g_order_content;
 
+SET TERM !!!;
+create trigger p_cust for CUSTOMERS
+BEFORE insert position 0
+as
+begin
+ IF (NEW.id IS NULL)
+    THEN NEW.ID = gen_id(g_customers,1);
+end!!!
+SET TERM; !!!
 
+SET TERM !!!;
+create trigger p_cust_ord for CUSTOMERS_ORDERS
+BEFORE insert position 0
+as
+begin
+ IF (NEW.id IS NULL)
+    THEN NEW.ID = gen_id(g_customers_orders,1);
+end!!!
+SET TERM; !!!
+
+SET TERM !!!;
+create trigger p_ord_cont for ORDER_CONTENT
+BEFORE insert position 0
+as
+begin
+ IF (NEW.id IS NULL)
+    THEN NEW.ID = gen_id(g_order_contents,1);
+end!!!
+SET TERM; !!!
 SET TERM !!!;
 create trigger p_bo for BOOKS
 BEFORE insert position 0
 as
 begin
- IF (NEW.id_spc IS NULL)
+ IF (NEW.id IS NULL)
     THEN NEW.ID = gen_id(g_books,1);
 end!!!
 SET TERM; !!!
@@ -19,7 +50,7 @@ create trigger p_au for AUTHORS
 BEFORE insert position 0
 as
 begin
- IF (NEW.id_spc IS NULL)
+ IF (NEW.id IS NULL)
     THEN NEW.ID = gen_id(g_authors,1);
 end!!!
 SET TERM; !!!
@@ -29,7 +60,7 @@ create trigger p_cat for BOOKS_CATEGORIES
 BEFORE insert position 0
 as
 begin
- IF (NEW.id_spc IS NULL)
+ IF (NEW.id IS NULL)
     THEN NEW.ID = gen_id(g_books_categories,1);
 end!!!
 SET TERM; !!!
@@ -39,7 +70,8 @@ create trigger p_boau for BOOKS_AUTHORS
 BEFORE insert position 0
 as
 begin
- IF (NEW.id_spc IS NULL)
+ IF (NEW.id IS NULL)
     THEN NEW.ID = gen_id(g_books_authors,1);
 end!!!
 SET TERM; !!!
+
